e253f089c76b2f0683bc8d28ebf2d7dd784809e5
nitaiklu <nitai.kluger@gmail.com>
2022-06-09 19:24:47 +0300

    ..

---
+++ Liquiduty_pool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+/// @title Liquidit_pair_pool Contract 
+
+contract Liquidity_pool {
+    // fields:
+    string public name1;
+    string public name2;
+    
+
+
+}
