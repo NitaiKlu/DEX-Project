198fee8bc4121c3175e5cd266c17e086cbdb5c39
Tom <tomazoulay@campus.technion.ac.il>
2022-06-27 22:33:07 +0300

    fixed bugs (there may be more updates)

--- Liquidity_pool.sol
+++ Liquidity_pool.sol
@@ -6,6 +6,7 @@ import "./PriceConsumerV3.sol";
 import "./ERC20_token/IERC20.sol";
 import "./PoolToken.sol";
 import "./Strings.sol";
+import "./Dex.sol";
 
 /// @title Liquidity_pair_pool Contract 
 
@@ -15,55 +16,49 @@ contract Liquidity_pool is Ownable {
     address public tokenB;
     address public liquidityToken;
     mapping (address => uint) donors;
+    address[] donorsKeys;
     uint private k_pool;
-    uint private total_funds_locked;
-    uint private total_funds_received;
+    //uint private total_funds_locked;
+    uint public total_funds_received;
+    uint private total_revenue;
+
     uint private amount_A;
     uint private amount_B;
-    PriceConsumerV3 internal priceFeedTokenA;
-    PriceConsumerV3 internal priceFeedTokenB;
+    address internal priceFeedTokenA;
+    address internal priceFeedTokenB;
     // onlyOwner() can be used at the end of a function
     // to make sure that only the owner of the contract can use the function
 
-    constructor(uint _initial_A, uint _initial_B,address _tokenA,address _tokenB, address _poolToken,address dataFeedA, address dataFeedB) {
+    constructor(address _tokenA, address _tokenB, address _poolToken, address _dataFeedA, address _dataFeedB) {
         tokenA = _tokenA;
         tokenB = _tokenB;
-        liquidityToken =_poolToken; 
+        liquidityToken = _poolToken; 
         amount_A = 0;
         amount_B = 0;
-        // TODO add the address of the token/USD feed we want to add 
         // for now i put  
-        // ETH/USD : 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        // BTC/USD : 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c
-        priceFeedTokenA = new PriceConsumerV3(dataFeedA);
-        priceFeedTokenB = new PriceConsumerV3(dataFeedB);
-        uint exchange_value_A;
-        exchange_value_A = uint256(priceFeedTokenA.getLatestPrice());
-        uint exchange_value_B;
-        exchange_value_B = uint256(priceFeedTokenB.getLatestPrice());
-        total_funds_received = _initial_A*exchange_value_A + _initial_B*exchange_value_B;
-        k_pool = amount_A * amount_B;
-    }
-    function assertOwnerAndFunds() public view returns (string memory)  {
-         uint exchange_value_A;
-        exchange_value_A = uint256(priceFeedTokenA.getLatestPrice());
-        uint exchange_value_B;
-        exchange_value_B = uint256(priceFeedTokenB.getLatestPrice());
-        string memory owner = Strings.toHexString(owner());
-        string memory fundsValue = Strings.toString(amount_A* exchange_value_A+ amount_B* exchange_value_B);
-        return Strings.concatenate("The owner of this contract is address",owner,". The total funds locked in this contract is",fundsValue);
+        // DAI : 0x6B175474E89094C44Da98b954EedeAC495271d0F
+        // DAI/USD : 0xaed0c38402a5d19df6e4c03f4e2dced6e29c1ee9
+        // AAVE : 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9
+        // AAVE/USD : 0x547a514d5e3769680ce22b2361c10ea13619e8a9
+        priceFeedTokenA = _dataFeedA;
+        priceFeedTokenB = _dataFeedB;
+        total_funds_received = 0;
+        total_revenue = 0;
+        k_pool = 0;
     }
 
+
     function exchange(uint _amount, address _coin_received) payable external  {
-        require((_coin_received==tokenA) || (_coin_received==tokenB));
-        
+        require((_coin_received==tokenA) || (_coin_received==tokenB), "The pool does not support this coin");
+        require(IERC20(_coin_received).allowance(msg.sender,address(this)) >= _amount, "Not enough tokens have been allowed");
         // amount is the amount of the token the user is sending us
         // need to multiply then divide by 100 to get the 3% and not nother data type
         uint amount_minus_fee = uint((_amount*(0.97*100))/100);
+        uint fee = _amount - amount_minus_fee;
+        total_revenue+= fee;
         // send the 3% to all liquidity providers ?
-        if(_coin_received == tokenA){
-            require(IERC20(tokenA).allowance(msg.sender,address(this)) >= _amount, "Not enough tokens have been allowed");
-            // dy = dx*y/(x=dx)
+        if(_coin_received == tokenA){  
+            // dy = dx*y/(x+dx)
             uint amount_sent_back = (amount_minus_fee * amount_B)/(amount_A+ amount_minus_fee);
             require(amount_sent_back<amount_B,"not enough funds to perform transaction");
             //take the funds of the user
@@ -71,29 +66,29 @@ contract Liquidity_pool is Ownable {
             // send back the other coins
             IERC20(tokenB).transfer(msg.sender,amount_sent_back);
             //update state
-            amount_A+= _amount;
-            amount_B-= amount_sent_back;
+            amount_A += _amount;
+            amount_B -= amount_sent_back;
         }
         else{
-            require(IERC20(tokenB).allowance(msg.sender,address(this)) >= _amount, "Not enough tokens have been allowed");
-            // dy = dx*y/(x=dx)
-            uint amount_sent_back = (amount_minus_fee * amount_A)/(amount_B+ amount_minus_fee);
+            // dy = dx*y/(x+dx)
+            uint amount_sent_back = (amount_minus_fee * amount_A) / (amount_B+ amount_minus_fee);
             require(amount_sent_back<amount_A,"not enough funds to perform transaction");
             //take the funds of the user
-            IERC20(tokenA).transferFrom(msg.sender, address(this),_amount);
+            IERC20(tokenB).transferFrom(msg.sender, address(this),_amount);
             //send back the other coins
             IERC20(tokenA).transfer(msg.sender, amount_sent_back);
             //update state
             amount_A-= amount_sent_back;
             amount_B+= _amount;
-
         }
+        
     }
+    
     function donate(uint _amount_coin_A, uint _amount_coin_B) payable public {
-         uint exchange_value_A;
-        exchange_value_A = uint256(priceFeedTokenA.getLatestPrice());
+        uint exchange_value_A;
+        exchange_value_A = 2; //uint256(PriceConsumerV3(priceFeedTokenA).getLatestPrice());
         uint exchange_value_B;
-        exchange_value_B = uint256(priceFeedTokenB.getLatestPrice());
+        exchange_value_B = 1; //uint256(PriceConsumerV3(priceFeedTokenB).getLatestPrice());
         
         uint amount_coin_A_dollars = _amount_coin_A* exchange_value_A;
         uint amount_coin_B_dollars = _amount_coin_B* exchange_value_B;
@@ -104,43 +99,70 @@ contract Liquidity_pool is Ownable {
         IERC20(tokenA).transferFrom(msg.sender, address(this), _amount_coin_A);
         IERC20(tokenB).transferFrom(msg.sender, address(this), _amount_coin_B);
         // send him back liquidityTokens
-        IERC20(liquidityToken).transfer(msg.sender,amount_coin_A_dollars*2);
+        IERC20(liquidityToken).transferFrom(owner(),msg.sender,amount_coin_A_dollars*2);
         donors[msg.sender]+= amount_coin_A_dollars*2;
+        donorsKeys.push(msg.sender);
         //update state
         amount_A+= _amount_coin_A;
         amount_B+= _amount_coin_B;
         k_pool = amount_A * amount_B;
         total_funds_received+= amount_coin_A_dollars*2;
     }
+    
+    
+    
+    function distributeDividends() internal {
+        uint exchange_value_A;
+        exchange_value_A = 2; //uint256(PriceConsumerV3(priceFeedTokenA).getLatestPrice());
+        uint exchange_value_B;
+        exchange_value_B = 1; //uint256(PriceConsumerV3(priceFeedTokenB).getLatestPrice());
+        
+        // iterate over mapping
+        for(uint index=0; index<donorsKeys.length; index++) {
+            address donor = donorsKeys[index];
+            if(0 == donors[donor]){
+                continue;
+            }
+
+            uint share_pool = (donors[donor]/total_funds_received);
+            uint amount_to_send = share_pool * total_revenue;
+            uint to_send_A = amount_to_send/(2*exchange_value_A);
+            uint to_send_B = amount_to_send/(2*exchange_value_B);
+            //here the amount to send to a and b should have the same relation as the pool
+            amount_A-= to_send_A;
+            amount_B-= to_send_B;
+            IERC20(tokenA).transfer(msg.sender, to_send_A);
+            IERC20(tokenB).transfer(msg.sender, to_send_B);
+        }
+        k_pool = amount_A * amount_B;
+        total_revenue = 0;
+    }
+    
 
     function redeem(uint _amount) payable public {
-        require(IERC20(liquidityToken).allowance(msg.sender,address(this)) >= _amount, Strings.concatenate("Not enough", Strings.toHexString(liquidityToken) ,"have been allowed"));
+        require(IERC20(liquidityToken).allowance(msg.sender,address(this)) >= _amount,"Not enough liquidity Tokens (Burger Swap) have been allowed");
+        //require(IERC20(liquidityToken).allowance(msg.sender,address(this)) >= _amount,"failed");
         //assert the person claiming is the liquidity provider
-        require(donors[msg.sender]>=_amount);
+        require(donors[msg.sender]>=_amount, "You didn't contribute such an amount to the pool");
         uint share_pool = (_amount/total_funds_received);
         
-         uint exchange_value_A;
-        exchange_value_A = uint256(priceFeedTokenA.getLatestPrice());
+        uint exchange_value_A;
+        exchange_value_A = 2; //uint256(PriceConsumerV3(priceFeedTokenA).getLatestPrice());
         uint exchange_value_B;
-        exchange_value_B = uint256(priceFeedTokenB.getLatestPrice());
+        exchange_value_B = 1; //uint256(PriceConsumerV3(priceFeedTokenB).getLatestPrice());
         
         uint amount_to_send = share_pool * (amount_A*exchange_value_A + amount_B*exchange_value_B);
-        uint ratio_pool;
-        if(amount_A > amount_B) {
-            ratio_pool = amount_A / amount_B;
-        }
-        else{
-            ratio_pool = amount_B / amount_A;
-        }
         total_funds_received-= _amount;
         donors[msg.sender]-= _amount;
+        
         uint to_send_A = amount_to_send/(2*exchange_value_A);
         uint to_send_B = amount_to_send/(2*exchange_value_B);
         //here the amount to send to a and b should have the same relation as the pool
         amount_A-= to_send_A;
         amount_B-= to_send_B;
-        IERC20(tokenA).transfer(msg.sender, to_send_A);
-        IERC20(tokenB).transfer(msg.sender, to_send_B);
+        require(IERC20(liquidityToken).transferFrom(msg.sender, owner(), _amount),"Sending liquidity tokens failed");
+        require(IERC20(tokenA).transfer(msg.sender,to_send_A),"Sending coinA failed");
+        require(IERC20(tokenB).transfer(msg.sender,to_send_B),"Sending coinB failed");
         k_pool = amount_A * amount_B;
     }
 
--- PoolToken.sol
+++ PoolToken.sol
@@ -3,39 +3,51 @@ pragma solidity ^0.8.6;
 // use latest solidity version at time of writing, need not worry about overflow and underflow
 
 /// @title ERC20 Contract - a liquidity pool token 
-// import "./ERC20_token/IERC20.sol"
+import "./ERC20_token/IERC20.sol";
 //whoever constructs this contract is the only owner of the totalSupply of the tokens.
 //in this project, it's the job of the Liquidity_pool contract to do so!
-contract PoolToken {
+contract PoolToken is IERC20 {
 
     // My Variables
     string public name;
     string public symbol;
     uint256 public decimals;
-    uint256 public totalSupply;
+    uint256 public totalSupplyToken;
 
     // Keep track balances and allowances approved
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceAccount;
+    mapping(address => mapping(address => uint256)) public allowanceAccount;
 
     // Events - fire events on state changes etc
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    //event Transfer(address indexed from, address indexed to, uint256 value);
+    //event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _totalSupply; 
-        balanceOf[msg.sender] = totalSupply;
+        totalSupplyToken = _totalSupply; 
+        balanceAccount[msg.sender] = totalSupplyToken;
     }
 
+    
+    function balanceOf(address account) override external view returns (uint256) {
+        return balanceAccount[account];
+    }
+    
+    function allowance(address owner, address spender) override external view returns (uint256) {
+        return allowanceAccount[owner][spender];
+    }
+    function totalSupply() override external view returns (uint256) {
+        return totalSupplyToken;
+    }
+    
     /// @notice transfer amount of tokens to an address
     /// @param _to receiver of token
     /// @param _value amount value of token to send
     /// @return success as true, for transfer 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+    function transfer(address _to, uint256 _value) override external returns (bool success) {
+        require(balanceAccount[msg.sender] >= _value);
         _transfer(msg.sender, _to, _value);
         return true;
     }
@@ -49,8 +61,8 @@ contract PoolToken {
     function _transfer(address _from, address _to, uint256 _value) internal {
         // Ensure sending is to valid address! 0x0 address cane be used to burn() 
         require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
+        balanceAccount[_from] = balanceAccount[_from] - (_value);
+        balanceAccount[_to] = balanceAccount[_to] + (_value);
         emit Transfer(_from, _to, _value);
     }
 
@@ -60,9 +72,9 @@ contract PoolToken {
     /// @return true, success once address approved
     //  Emit the Approval event  
     // Allow _spender to spend up to _value on your behalf
-    function approve(address _spender, uint256 _value) external returns (bool) {
+    function approve(address _spender, uint256 _value) override external returns (bool) {
         require(_spender != address(0));
-        allowance[msg.sender][_spender] = _value;
+        allowanceAccount[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -74,10 +86,10 @@ contract PoolToken {
     /// @dev internal helper transfer function with required safety checks
     /// @return true, success once transfered from original account    
     // Allow _spender to spend up to _value on your behalf
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - (_value);
+    function transferFrom(address _from, address _to, uint256 _value) override external returns (bool) {
+        require(_value <= balanceAccount[_from]);
+        require(_value <= allowanceAccount[_from][msg.sender]);
+        allowanceAccount[_from][msg.sender] = allowanceAccount[_from][msg.sender] - (_value);
         _transfer(_from, _to, _value);
         return true;
     }
